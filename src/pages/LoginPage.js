02b4940b91157786290f857c773e7e19d601dc62
ahmad420 <ahmdaf420@gmail.com>
2021-02-23 00:12:37 +0200

    conneced with database

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+
+const LoginPage = () => {
+  return <></>
+}
+
+export default LoginPage
--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import products from '../products.js'
+// import products from '../products.js'
 import Rating from '../components/Rating'
-
+import axios from 'axios'
 const ProductsPage = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
 
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [match])
   return (
     <>
       <Link className='btn btn-dark my-3' to='/'>
